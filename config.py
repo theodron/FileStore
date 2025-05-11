ac69f8a6302565c908fed1d8aaf437b62f9954e5
theodron <theodronorg@gmail.com>
2025-05-11 16:38:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/ec17880d61180d3
 FORCE_PIC = os.environ.get("FORCE_PIC", "https://telegra.ph/file/e292b12890b8b4b9dcbd1.jpg")
 
 #--------------------------------------------
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "yourdestinationlink.com")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "shortner.in")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "a4ee232968a14093fb771e2eb5a1bf6c5558ac54")
 TUT_VID = os.environ.get("TUT_VID","https://t.me/hwdownload/3")
 SHORT_MSG = "<b>⌯ Here is Your Download Link, Must Watch Tutorial Before Clicking On Download...</b>"
